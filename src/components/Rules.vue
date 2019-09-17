9b8128e3e1e2f2a0e80a84f0a17d836e171d122e
backmeupplz <backmeupplz@gmail.com>
2019-09-17 10:36:05 -0700

    add a rule

--- src/components/Rules.vue
+++ src/components/Rules.vue
@@ -7,7 +7,7 @@
       v-card-title {{$t('howto.title')}}
       v-card-text
         ul
-          li(v-for='i in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]' v-html='$t(`howto.rules.${i}`)')
+          li(v-for='i in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]' v-html='$t(`howto.rules.${i}`)')
         .title.py-2 {{$t('howto.bonus.title')}}
         ul
           li(v-for='i in [0]' v-html='$t(`howto.bonus.${i}`)')
