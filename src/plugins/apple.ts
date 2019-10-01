6d84ad04673d8c56bb119fafd92cd7b15f3ad033
backmeupplz <backmeupplz@gmail.com>
2019-09-30 17:35:28 -0700

    add redirect

--- src/plugins/apple.ts
+++ src/plugins/apple.ts
@@ -5,6 +5,7 @@ import VueAppleSignin from 'vue-apple-signin'
 Vue.use(VueAppleSignin, {
   clientId: process.env.VUE_APP_APPLE_CLIENT_ID,
   scope: 'name email',
-  redirectURI: process.env.VUE_APP_WEBSITE,
+  // redirectURI: `${process.env.VUE_APP_API}/apple`,
+  redirectURI: `https://cc9ec665.ngrok.io/apple`,
   state: 'nostate',
 })
