c7279c27e9ae49e3b9139852c6c88444aec4d776
backmeupplz <backmeupplz@gmail.com>
2019-12-15 09:38:42 -0800

    fix dialog

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -131,7 +131,7 @@ declare const FB: any;
   }
 })
 export default class Home extends Vue {
-  signinDialog = true;
+  signinDialog = false;
 
   created() {
     if (this.$route.query && this.$route.query.hash) {
