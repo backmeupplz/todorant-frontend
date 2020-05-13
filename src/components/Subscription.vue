598d90322b7396a5882f525f5afa31662b275299
backmeupplz <backmeupplz@gmail.com>
2020-05-13 15:47:04 -0700

    fix locales

--- src/components/Subscription.vue
+++ src/components/Subscription.vue
@@ -14,10 +14,10 @@
           a(href='mailto:nikita@borodutch.com') {{$t('subscription.signature')}}
         .text-center(v-if='!$store.state.userState.subscriptionIdExists')
           v-btn.ma-2(:loading='loading'
-          @click='redirectToPurchase("monthly")') {{$t('subscription.5')}}
+          @click='redirectToPurchase("monthly")') {{$t('subscription.5dollars')}}
           v-btn.ma-2(color='primary'
           :loading='loading'
-          @click='redirectToPurchase("yearly")') {{$t('subscription.50')}}
+          @click='redirectToPurchase("yearly")') {{$t('subscription.50dollars')}}
       v-card-actions
         v-spacer
         v-btn(v-if='$store.state.userState.subscriptionIdExists'
@@ -34,28 +34,28 @@
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import Component from "vue-class-component";
-import { i18n } from "../plugins/i18n";
-import * as store from "../plugins/store";
-import { daysBetween } from "../utils/daysBetween";
-import * as api from "../utils/api";
-import { serverBus } from "../main";
-import { reportGA } from "../utils/ga";
+import Vue from 'vue'
+import Component from 'vue-class-component'
+import { i18n } from '../plugins/i18n'
+import * as store from '../plugins/store'
+import { daysBetween } from '../utils/daysBetween'
+import * as api from '../utils/api'
+import { serverBus } from '../main'
+import { reportGA } from '../utils/ga'
 
 // Stripe object is global, declaring here for TS
-declare const Stripe: any;
+declare const Stripe: any
 
-const stripe = Stripe(process.env.VUE_APP_STRIPE);
+const stripe = Stripe(process.env.VUE_APP_STRIPE)
 
 @Component({
   props: {
     dialog: Boolean,
-    close: Function
-  }
+    close: Function,
+  },
 })
 export default class Subscription extends Vue {
-  loading = false;
+  loading = false
 
   get subscriptionStatusText() {
     if (
@@ -64,79 +64,79 @@ export default class Subscription extends Vue {
     ) {
       return `${i18n.t(
         `subscription.${store.userState().subscriptionStatus}`
-      )} ${i18n.t("subscription.daysLeft", {
+      )} ${i18n.t('subscription.daysLeft', {
         daysLeft:
-          30 - daysBetween(new Date(store.userState().createdAt), new Date())
-      })}`;
+          30 - daysBetween(new Date(store.userState().createdAt), new Date()),
+      })}`
     } else {
-      return i18n.t(`subscription.${store.userState().subscriptionStatus}`);
+      return i18n.t(`subscription.${store.userState().subscriptionStatus}`)
     }
   }
   get subscriptionDescriptionText() {
     return i18n.t(
       `subscription.${store.userState().subscriptionStatus}Description`
-    );
+    )
   }
 
   async redirectToPurchase(plan: api.Plan) {
-    const user = store.user();
+    const user = store.user()
     if (!user) {
-      return;
+      return
     }
-    this.loading = true;
+    this.loading = true
     try {
-      const session = await api.getPlanSession(user, plan);
+      const session = await api.getPlanSession(user, plan)
       const result = await stripe.redirectToCheckout({
-        sessionId: session.session
-      });
+        sessionId: session.session,
+      })
       if (result.error) {
-        store.setSnackbarError(result.error.message);
-        return;
+        store.setSnackbarError(result.error.message)
+        return
       }
       store.setSnackbar({
-        message: i18n.t("subscription.success") as string,
+        message: i18n.t('subscription.success') as string,
         active: true,
-        color: "success"
-      });
-      (this as any).close();
-      reportGA("subscription_success", {
-        status: store.userState().subscriptionStatus
-      });
-    } catch (err) {
-      store.setSnackbarError(err.message);
-      reportGA("subscription_purchase_error", {
+        color: 'success',
+      })
+      ;(this as any).close()
+      reportGA('subscription_success', {
         status: store.userState().subscriptionStatus,
-        error: err.message
-      });
+      })
+    } catch (err) {
+      store.setSnackbarError(err.message)
+      reportGA('subscription_purchase_error', {
+        status: store.userState().subscriptionStatus,
+        error: err.message,
+      })
     } finally {
-      this.loading = false;
+      this.loading = false
     }
   }
 
   async cancelSubscription() {
-    const user = store.user();
+    const user = store.user()
     if (!user) {
-      return;
+      return
     }
-    this.loading = true;
+    this.loading = true
     try {
-      if (!confirm(i18n.t("subscription.cancelConfirm") as string)) {
-        return;
+      if (!confirm(i18n.t('subscription.cancelConfirm') as string)) {
+        return
       }
-      const session = await api.cancelSubscription(user);
-      (this as any).close();
-      serverBus.$emit("refreshRequested");
-      reportGA("subscription_canceled", {
-        status: store.userState().subscriptionStatus
-      });
-    } catch (err) {
-      store.setSnackbarError(err.message);
-      reportGA("subscription_cancel_error", {
+      const session = await api.cancelSubscription(user)
+      ;(this as any).close()
+      serverBus.$emit('refreshRequested')
+      reportGA('subscription_canceled', {
         status: store.userState().subscriptionStatus,
-        error: err.message
-      });
+      })
+    } catch (err) {
+      store.setSnackbarError(err.message)
+      reportGA('subscription_cancel_error', {
+        status: store.userState().subscriptionStatus,
+        error: err.message,
+      })
     } finally {
-      this.loading = false;
+      this.loading = false
     }
   }
 }
