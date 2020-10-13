dfeb1f9cf8800d212c3c5b32975698a36f942ca7
Tasumaniadiabori <tasumaniadiabori@protonmail.com>
2020-10-13 22:06:44 +0300

    Add an option to add delegation task without date or month

--- src/views/AddTodo.vue
+++ src/views/AddTodo.vue
@@ -256,7 +256,7 @@ export default class AddTodo extends Vue {
         !todo ||
         !todo.text ||
         !todo.text.trim() ||
-        (!todo.monthAndYear && !todo.date)
+        (!todo.monthAndYear && !todo.date && !todo.delegate)
       ) {
         this.panel.push(i)
       }
