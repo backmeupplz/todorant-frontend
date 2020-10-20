70f72279c02ecbc3968a041948b4a3e0349c28d8
upacyxou <upacyxou@tutanota.com>
2020-10-20 19:14:20 +0400

    improve page load speed

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,16 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin
+
+const plugins = []
+if (process.env.VUE_APP_BUNDLE === 'true') {
+  plugins.push(new BundleAnalyzerPlugin())
+}
+
 module.exports = {
   devServer: {
-    disableHostCheck: true
-  }
+    disableHostCheck: true,
+  },
+  configureWebpack: {
+    plugins,
+  },
 }
