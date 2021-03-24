1d997c5c25dbbb19c9837fbbabc2533f0ec5caaa
upacyxou <upacyxou@tutanota.com>
2021-03-24 23:39:06 +0400

    fix sharing links

--- src/views/Report.vue
+++ src/views/Report.vue
@@ -190,13 +190,13 @@ const AppStore = namespace('AppStore')
   },
 })
 export default class Report extends Vue {
-  @Prop({ default: false }) external!: boolean
-
   @AppStore.State dark!: boolean
   @UserStore.State user?: User
   @AppStore.State language?: string
   @SettingsStore.State firstDayOfWeek?: number
   @SnackbarStore.Mutation setSnackbarError!: (error: string) => void
+
+  external = false
 
   loading = false
 
@@ -220,6 +220,10 @@ export default class Report extends Vue {
 
   endDateMenu = false
   endDate: null | string = null
+
+  created() {
+    this.external = this.$router.currentRoute.name === 'public_report'
+  }
 
   mounted() {
     this.refresh()
@@ -324,9 +328,6 @@ export default class Report extends Vue {
 
   async refresh() {
     const user = this.user
-    if (!user && !this.external) {
-      return
-    }
     this.loading = true
     try {
       let data: any
