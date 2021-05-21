225b1d36bbc74cbcaa68962ba2267eea63e5d22b
upacyxou <upacyxou@tutanota.com>
2021-05-21 20:50:10 +0400

    trying to fix socket connection

--- src/utils/sockets/socketIO.ts
+++ src/utils/sockets/socketIO.ts
@@ -1,5 +1,11 @@
 import SocketIO from 'socket.io-client'
 
 export const socketIO = SocketIO(
-  process.env.VUE_APP_DEV ? 'http://localhost:3000' : 'https://ws.todorant.com'
+  process.env.VUE_APP_DEV ? 'http://localhost:3000' : 'https://ws.todorant.com',
+  {
+    reconnection: true,
+    reconnectionAttempts: Infinity,
+    reconnectionDelay: 1000,
+    reconnectionDelayMax: 30000,
+  }
 )
