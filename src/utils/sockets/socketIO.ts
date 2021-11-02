84d1c03a07e830e9e6e38668224996ff0fe4194f
Ruslan Zapevalov <upacyxou@tutanota.com>
2021-11-02 17:15:49 +0400

    start refactoring hotkeys

--- src/utils/sockets/socketIO.ts
+++ src/utils/sockets/socketIO.ts
@@ -1,5 +1,5 @@
 import SocketIO from 'socket.io-client'
 
 export const socketIO = SocketIO(
-  process.env.VUE_APP_DEV ? 'http://localhost:3000' : 'https://ws.todorant.com'
+  !process.env.VUE_APP_DEV ? 'http://localhost:3000' : 'https://ws.todorant.com'
 )
