5cf55298ae7368515b8abc3388492b2239e10b70
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-05-15 16:29:02 -0700

    add privacy

--- src/views/static/Privacy.vue
+++ src/views/static/Privacy.vue
@@ -24,6 +24,8 @@
   p Our Service may contain links to other sites. If you click on a third-party link, you will be directed to that site. Note that these external sites are not operated by us. Therefore, we strongly advise you to review the Privacy Policy of these websites. We have no control over, and assume no responsibility for the content, privacy policies, or practices of any third-party sites or services.
   h3 Children’s Privacy
   p Our Services do not address anyone under the age of 13. We do not knowingly collect personal identifiable information from children under 13. In the case we discover that a child under 13 has provided us with personal information, we immediately delete this from our servers. If you are a parent or guardian and you are aware that your child has provided us with personal information, please contact us so that we will be able to do necessary actions.
+  h3 Accounts
+  p To retrieve or delete your account data, please, send the request to accounts@todorant.com.
   h3 Changes to This Privacy Policy
   p We may update our Privacy Policy from time to time. Thus, we advise you to review this page periodically for any changes. We will notify you of any changes by posting the new Privacy Policy on this page. These changes are effective immediately, after they are posted on this page.
   h3 Contact Us
